2b3c3755b6fdc02da41c8faed00c8df1223157fd
noy1224 <167700446+noy1224@users.noreply.github.com>
2025-02-26 02:11:33 +0900

    a

--- app/_components/Category/index.tsx
+++ app/_components/Category/index.tsx
@@ -5,5 +5,8 @@ type Props = {
     category: Category;
 };
 export default function Category({ category }: Props) {
+  if (!category) {
+    return null; // categoryがない場合は何も表示しない
+  }
   return <span className={styles.tag}>{category.name}</span>;
 }
